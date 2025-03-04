1743eec963b5926b8f0240bfa6b720fc667199c9
Hxzardd <darsheelchahar22@gmail.com>
2025-03-04 22:42:06 +0530

    Finished Level 3 (well, actually couldn't)

---
+++ cc/level3.py
@@ -0,0 +1,4 @@
+import heapq
+
+# All I was able to understand was that we would use min-heap for the retrieval of the first expiring key (earlier)
+# Sources referred to - https://www.geeksforgeeks.org/heap-queue-or-heapq-in-python/, https://www.geeksforgeeks.org/insertion-and-deletion-in-heaps/ and ofc chatgpt...
